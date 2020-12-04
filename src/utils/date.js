1aacc73aefd946f441e651099f680b4f12fed981
zhaodanning <zhao.zdn@alibaba-inc.com>
2020-12-04 23:20:07 +0800

    时间

--- src/utils/date.js
+++ src/utils/date.js
@@ -3,14 +3,14 @@ import dayjs from 'dayjs'
 const getDate = (date) => {
   if(new Date(date).toDateString() === new Date().toDateString()){
     // return `今天 ${new Date().toLocaleString('chinese', { hour12: false }).replace(/\//g,'-').split(' ')[1].substr(0,5)}`
-    return `今天 ${dayjs().format("MM-DD hh:mm")}`
+    return `今天 ${dayjs(date).format("MM-DD HH:mm")}`
   }else{
-    return dayjs().format("MM-DD HH:mm")
+    return dayjs(date).format("MM-DD HH:mm")
   }
 }
 
 const getDayTime = (date) => {
-  return dayjs().format("YYYY-MM-DD HH:mm")
+  return dayjs(date).format("YYYY-MM-DD HH:mm")
 }
 
 export {
