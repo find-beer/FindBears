5a6987dc3800d9a95a39ec7b9213b37a51ad0425
zhaodanning <zhao.zdn@alibaba-inc.com>
2020-12-19 15:26:57 +0800

    动态详情

--- src/pages/home/dynamic_detail/photo.js
+++ src/pages/home/dynamic_detail/photo.js
@@ -7,19 +7,15 @@ export default props => {
     const picUrl = data
     return (
         <View style={styles.container}>
-            <Image
-                source={{uri:'https://t-bear-test.oss-cn-beijing.aliyuncs.com/images/2020/11/21/1_1605934788229_6478.jpg'}}
-                style={styles.photo}
-            ></Image>
-            {/* {(picUrl? picUrl.split(','):[]).map((item, index) => {
+            {(picUrl? picUrl.split(','):[]).map((item, index) => {
                 return (
                     <Image
                         key={index}
-                        source={{'uri':'https://t-bear-test.oss-cn-beijing.aliyuncs.com/images/2020/11/21/1_1605934788229_6478.jpg'}}
+                        source={{uri:item}}
                         style={styles.photo}
                     ></Image>
                 );
-            })} */}
+            })}
         </View>
     );
 };
